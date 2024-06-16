9e89f28aacfe2921a715e6cf257ade0a960aab43
Soraia Novaes <sonovaes511@gmail.com>
2024-06-16 17:54:32 -0300

    Adjust user params on clerk route

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -8,109 +8,122 @@ import { Webhook } from "svix";
 import { createUser, deleteUser, updateUser } from "@/lib/actions/user.actions";
 
 export async function POST(req: Request) {
-  // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
-  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+	// You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
+	const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
 
-  if (!WEBHOOK_SECRET) {
-    throw new Error(
-      "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
-    );
-  }
+	if (!WEBHOOK_SECRET) {
+		throw new Error(
+			"Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
+		);
+	}
 
-  // Get the headers
-  const headerPayload = headers();
-  const svix_id = headerPayload.get("svix-id");
-  const svix_timestamp = headerPayload.get("svix-timestamp");
-  const svix_signature = headerPayload.get("svix-signature");
+	// Get the headers
+	const headerPayload = headers();
+	const svix_id = headerPayload.get("svix-id");
+	const svix_timestamp = headerPayload.get("svix-timestamp");
+	const svix_signature = headerPayload.get("svix-signature");
 
-  // If there are no headers, error out
-  if (!svix_id || !svix_timestamp || !svix_signature) {
-    return new Response("Error occured -- no svix headers", {
-      status: 400,
-    });
-  }
+	// If there are no headers, error out
+	if (!svix_id || !svix_timestamp || !svix_signature) {
+		return new Response("Error occured -- no svix headers", {
+			status: 400,
+		});
+	}
 
-  // Get the body
-  const payload = await req.json();
-  const body = JSON.stringify(payload);
+	// Get the body
+	const payload = await req.json();
+	const body = JSON.stringify(payload);
 
-  // Create a new Svix instance with your secret.
-  const wh = new Webhook(WEBHOOK_SECRET);
+	// Create a new Svix instance with your secret.
+	const wh = new Webhook(WEBHOOK_SECRET);
 
-  let evt: WebhookEvent;
+	let evt: WebhookEvent;
 
-  // Verify the payload with the headers
-  try {
-    evt = wh.verify(body, {
-      "svix-id": svix_id,
-      "svix-timestamp": svix_timestamp,
-      "svix-signature": svix_signature,
-    }) as WebhookEvent;
-  } catch (err) {
-    console.error("Error verifying webhook:", err);
-    return new Response("Error occured", {
-      status: 400,
-    });
-  }
+	// Verify the payload with the headers
+	try {
+		evt = wh.verify(body, {
+			"svix-id": svix_id,
+			"svix-timestamp": svix_timestamp,
+			"svix-signature": svix_signature,
+		}) as WebhookEvent;
+	} catch (err) {
+		console.error("Error verifying webhook:", err);
+		return new Response("Error occured", {
+			status: 400,
+		});
+	}
 
-  // Get the ID and type
-  const { id } = evt.data;
-  const eventType = evt.type;
+	// Get the ID and type
+	const { id } = evt.data;
+	const eventType = evt.type;
 
-  // CREATE
-  if (eventType === "user.created") {
-    const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
+	// CREATE
+	if (eventType === "user.created") {
+		const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
 
-    const user = {
-      clerkId: id,
-      email: email_addresses[0].email_address,
-      username: username!,
-      firstName: first_name,
-      lastName: last_name,
-      photo: image_url,
-    };
+		const user = {
+			clerkId: id,
+			email: email_addresses[0].email_address,
+			username: username!,
+			firstName: first_name,
+			lastName: last_name,
+			photo: image_url,
+		};
 
-    const newUser = await createUser(user);
+		// Ajustar as propriedades para garantir que são strings
+		const adjustedUser = {
+			...user,
+			firstName: user.firstName ?? "",
+			lastName: user.lastName ?? "",
+		};
 
-    // Set public metadata
-    if (newUser) {
-      await clerkClient.users.updateUserMetadata(id, {
-        publicMetadata: {
-          userId: newUser._id,
-        },
-      });
-    }
+		const newUser = await createUser(adjustedUser);
 
-    return NextResponse.json({ message: "OK", user: newUser });
-  }
+		// Set public metadata
+		if (newUser) {
+			await clerkClient.users.updateUserMetadata(id, {
+				publicMetadata: {
+					userId: newUser._id,
+				},
+			});
+		}
 
-  // UPDATE
-  if (eventType === "user.updated") {
-    const { id, image_url, first_name, last_name, username } = evt.data;
+		return NextResponse.json({ message: "OK", user: newUser });
+	}
 
-    const user = {
-      firstName: first_name,
-      lastName: last_name,
-      username: username!,
-      photo: image_url,
-    };
+	// UPDATE
+	if (eventType === "user.updated") {
+		const { id, image_url, first_name, last_name, username } = evt.data;
 
-    const updatedUser = await updateUser(id, user);
+		const user = {
+			firstName: first_name,
+			lastName: last_name,
+			username: username!,
+			photo: image_url,
+		};
+		// Ajustar as propriedades para garantir que são strings
+		const adjustedUser = {
+			...user,
+			firstName: user.firstName ?? "",
+			lastName: user.lastName ?? "",
+		};
 
-    return NextResponse.json({ message: "OK", user: updatedUser });
-  }
+		const updatedUser = await updateUser(id, adjustedUser);
 
-  // DELETE
-  if (eventType === "user.deleted") {
-    const { id } = evt.data;
+		return NextResponse.json({ message: "OK", user: updatedUser });
+	}
 
-    const deletedUser = await deleteUser(id!);
+	// DELETE
+	if (eventType === "user.deleted") {
+		const { id } = evt.data;
 
-    return NextResponse.json({ message: "OK", user: deletedUser });
-  }
+		const deletedUser = await deleteUser(id!);
 
-  console.log(`Webhook with and ID of ${id} and type of ${eventType}`);
-  console.log("Webhook body:", body);
+		return NextResponse.json({ message: "OK", user: deletedUser });
+	}
 
-  return new Response("", { status: 200 });
+	console.log(`Webhook with and ID of ${id} and type of ${eventType}`);
+	console.log("Webhook body:", body);
+
+	return new Response("", { status: 200 });
 }
